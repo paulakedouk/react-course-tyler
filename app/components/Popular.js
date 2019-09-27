6638fc9c250a948f374f543d67fc17291c0a4aba
paulakedouk <paulakedouk@gmail.com>
2019-09-27 12:58:54 -0700

    Catching repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,14 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 
-function LanguagesNav({ selected, onUpdateLanguage }) {
+function LangaugesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-  return (
 
+  return (
     <ul className='flex-center'>
       {languages.map((language) => (
         <li key={language}>
-          <button className='btn-clear nav-link'
+          <button
+            className='btn-clear nav-link'
             style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
             onClick={() => onUpdateLanguage(language)}>
             {language}
@@ -20,68 +21,79 @@ function LanguagesNav({ selected, onUpdateLanguage }) {
   )
 }
 
-LanguagesNav.propTypes = {
+LangaugesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
 }
 
 export default class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       selectedLanguage: 'All',
-      repos: null,
-      error: null
+      repos: {},
+      error: null,
     }
 
     this.updateLanguage = this.updateLanguage.bind(this)
     this.isLoading = this.isLoading.bind(this)
   }
-
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage)
   }
-
   updateLanguage(selectedLanguage) {
     this.setState({
       selectedLanguage,
-      repos: null,
-      error: null
+      error: null,
     })
 
-    fetchPopularRepos(selectedLanguage)
-      .then((repos) => this.setState({
-        repos,
-        error: null
-      }))
-      .catch(() => {
-        console.warn('Error fetching repos: ', error)
-
-        this.setState({
-          error: 'There was an error fetching the repositories.'
+    // If whatever these selected language is, doesn't  already exist 
+    // in our repos object, then we want to go ahead and fetch the popula repository
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }))
         })
-      })
+        .catch(() => {
+          console.warn('Error fetching repos: ', error)
 
+          this.setState({
+            error: `There was an error fetching the repositories.`
+          })
+        })
+    }
   }
-
+  // Out component is going to be loading or it's going to be still fetching
+  // the data if repos at selectedLanguage is falsy, meaning it's not a thing yet. 
+  // We haven't already gone and fetched those repositories and error equals null.
   isLoading() {
-    return this.state.repos === null && this.state.error === null
-  }
+    const { selectedLanguage, repos, error } = this.state
 
+    return !repos[selectedLanguage] && error === null
+  }
   render() {
     const { selectedLanguage, repos, error } = this.state
 
     return (
-      <>
-        <LanguagesNav
+      <React.Fragment>
+        <LangaugesNav
           selected={selectedLanguage}
-          onUpdateLanguage={this.updateLanguage} />
+          onUpdateLanguage={this.updateLanguage}
+        />
 
         {this.isLoading() && <p>LOADING</p>}
+
         {error && <p>{error}</p>}
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
-      </>
+        {/* We only want to show this, if repos at a selectedLanguage is a thing. */}
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+      </React.Fragment>
     )
   }
 }
+
