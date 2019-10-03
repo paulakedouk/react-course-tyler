cd7e07be8ebded59d93aa3ef2118d3d3822269da
paulakedouk <paulakedouk@gmail.com>
2019-10-03 12:17:52 -0700

    Reusable Card

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,9 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
+import Card from './Card'
 
-
-function LangaugesNav({ selected, onUpdateLanguage }) {
+function LangaugesNav ({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
   return (
@@ -28,7 +28,7 @@ LangaugesNav.propTypes = {
   onUpdateLanguage: PropTypes.func.isRequired
 }
 
-function ReposGrid({ repos }) {
+function ReposGrid ({ repos }) {
   return (
     <ul className='grid space-around'>
       {repos.map((repo, index) => {
@@ -36,38 +36,34 @@ function ReposGrid({ repos }) {
         const { login, avatar_url } = owner
 
         return (
-          <li key={html_url} className='repo bg-light'>
-            <h4 className='header-lg center-text'>
-              #{index + 1}
-            </h4>
-            <img
-              className='avatar'
-              src={avatar_url}
-              alt={`Avatar for ${login}`}
-            />
-            <h2 className='center-text'>
-              <a className='link' href={html_url}>{login}</a>
-            </h2>
-            <ul className='card-list'>
-              <li>
-                <FaUser color='rgb(255, 191, 116)' size={22} />
-                <a href={`https://github.com/${login}`}>
-                  {login}
-                </a>
-              </li>
-              <li>
-                <FaStar color='rgb(255, 215, 0)' size={22} />
-                {stargazers_count.toLocaleString()} stars
-              </li>
-              <li>
-                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-                {forks.toLocaleString()} forks
-              </li>
-              <li>
-                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                {open_issues.toLocaleString()} open
-              </li>
-            </ul>
+          <li key={html_url}>
+            <Card
+              header={`#${index + 1}`}
+              avatar={avatar_url}
+              href={html_url}
+              name={login}
+            >
+              <ul className='card-list'>
+                <li>
+                  <FaUser color='rgb(255, 191, 116)' size={22} />
+                  <a href={`https://github.com/${login}`}>
+                    {login}
+                  </a>
+                </li>
+                <li>
+                  <FaStar color='rgb(255, 215, 0)' size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
           </li>
         )
       })}
@@ -92,17 +88,15 @@ export default class Popular extends React.Component {
     this.updateLanguage = this.updateLanguage.bind(this)
     this.isLoading = this.isLoading.bind(this)
   }
-  componentDidMount() {
+  componentDidMount () {
     this.updateLanguage(this.state.selectedLanguage)
   }
-  updateLanguage(selectedLanguage) {
+  updateLanguage (selectedLanguage) {
     this.setState({
       selectedLanguage,
       error: null,
     })
 
-    // If whatever these selected language is, doesn't  already exist 
-    // in our repos object, then we want to go ahead and fetch the popula repository
     if (!this.state.repos[selectedLanguage]) {
       fetchPopularRepos(selectedLanguage)
         .then((data) => {
@@ -122,9 +116,6 @@ export default class Popular extends React.Component {
         })
     }
   }
-  // Out component is going to be loading or it's going to be still fetching
-  // the data if repos at selectedLanguage is falsy, meaning it's not a thing yet. 
-  // We haven't already gone and fetched those repositories and error equals null.
   isLoading() {
     const { selectedLanguage, repos, error } = this.state
 
@@ -142,11 +133,10 @@ export default class Popular extends React.Component {
 
         {this.isLoading() && <p>LOADING</p>}
 
-        {error && <p>{error}</p>}
-        {/* We only want to show this, if repos at a selectedLanguage is a thing. */}
+        {error && <p className='center-text error'>{error}</p>}
+
         {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
       </React.Fragment>
     )
   }
 }
-
